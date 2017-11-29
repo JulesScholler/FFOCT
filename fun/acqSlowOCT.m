63abede42713d8f0d4c90f2ede1bd2cfd2edaa37
Jules Scholler <jules.scholler@gmail.com>
2017-11-29 16:59:41 +0100

    correct error on save

--- fun/acqSlowOCT.m
+++ fun/acqSlowOCT.m
@@ -11,7 +11,11 @@ mkdir([handles.save.path '\' handles.save.t ])
 
 set(handles.octCam.vid, 'TriggerFrameDelay', 10) % We leave the first 10 frames because the camera is not stable
 
+data=[];
+handles.save.timeOCT=[];
+h=waitbar(0,'Acquistion in progess, please wait.');
 for i=1:handles.save.repeatN
+    tic
     switch handles.exp.piezoMode
         case 1 % Direct image only
             handles.exp.FramesPerTrigger=handles.octCam.Naccu*handles.save.N;
@@ -29,7 +33,7 @@ for i=1:handles.save.repeatN
             [data_tmp,timeOCT_tmp]=getdata(handles.octCam.vid,handles.exp.FramesPerTrigger,'double');
             stop(handles.octCam.vid);
             stop(handles.DAQ.s);
-            data=cat(3,data,data_tmp);
+            data=cat(4,data,data_tmp);
             handles.save.timeOCT=cat(1,handles.save.timeOCT,timeOCT_tmp);
         case 2 % Two phases imaging
             handles.exp.FramesPerTrigger=2*handles.octCam.Naccu*handles.save.N;
@@ -47,7 +51,7 @@ for i=1:handles.save.repeatN
             [data,handles.save.timeOCT]=getdata(handles.octCam.vid,handles.exp.FramesPerTrigger,'double');
             stop(handles.octCam.vid);
             stop(handles.DAQ.s);
-            data=cat(3,data,data_tmp);
+            data=cat(4,data,data_tmp);
             handles.save.timeOCT=cat(1,handles.save.timeOCT,timeOCT_tmp);
         case 3 % 4 phase imaging
             handles.exp.FramesPerTrigger=4*handles.octCam.Naccu*handles.save.N;
@@ -65,10 +69,12 @@ for i=1:handles.save.repeatN
             [data,handles.save.timeOCT]=getdata(handles.octCam.vid,handles.exp.FramesPerTrigger,'double');
             stop(handles.octCam.vid);
             stop(handles.DAQ.s);
-            data=cat(3,data,data_tmp);
+            data=cat(4,data,data_tmp);
             handles.save.timeOCT=cat(1,handles.save.timeOCT,timeOCT_tmp);
         case 4 % 5 phases
     end
+    waitbar(i/handles.save.repeatN)
+    pause(handles.save.repeatTime-toc)
 end
 
 switch handles.exp.piezoMode
@@ -78,14 +84,14 @@ switch handles.exp.piezoMode
         end
         if handles.save.direct
             direct=zeros(size(data,1),size(data,2),handles.save.N);
-            for i=1:handles.save.N
+            for i=1:handles.save.N*handles.save.repeatN
                 direct(:,:,i)=mean(data(:,:,1,(i-1)*handles.octCam.Naccu+1:i*handles.octCam.Naccu),4);
             end
             saveAsTiff(direct,'direct','adimec',handles)
         end
     case 2 % Two phases imaging
         imTomo=zeros(size(data,1),size(data,2),handles.save.N);
-        for i=1:handles.save.N
+        for i=1:handles.save.N*handles.save.repeatN
             imTomo(:,:,i)=abs(mean(data(:,:,1,2*(i-1)*handles.octCam.Naccu+1:2:2*i*handles.octCam.Naccu),4)-mean(data(:,:,1,2*(i-1)*handles.octCam.Naccu+2:2:2*i*handles.octCam.Naccu),4));
         end
         if handles.save.allraw
@@ -93,7 +99,7 @@ switch handles.exp.piezoMode
         end
         if handles.save.direct
             direct=zeros(size(data,1),size(data,2),handles.save.N);
-            for i=1:handles.save.N
+            for i=1:handles.save.N*handles.save.repeatN
                 direct(:,:,i)=mean(data(:,:,1,2*(i-1)*handles.octCam.Naccu+1:2:2*i*handles.octCam.Naccu),4);
             end
             saveAsTiff(direct,'direct','adimec',handles)
@@ -106,7 +112,7 @@ switch handles.exp.piezoMode
         I2=I1;
         I3=I1;
         I4=I1;
-        for i=1:handles.save.N
+        for i=1:handles.save.N*handles.save.repeatN
             I1(:,:,i)=mean(data(:,:,1,4*(i-1)*handles.octCam.Naccu+1:4:4*i*handles.octCam.Naccu),4);
             I2(:,:,i)=mean(data(:,:,1,4*(i-1)*handles.octCam.Naccu+2:4:4*i*handles.octCam.Naccu),4);
             I3(:,:,i)=mean(data(:,:,1,4*(i-1)*handles.octCam.Naccu+3:4:4*i*handles.octCam.Naccu),4);
@@ -119,7 +125,7 @@ switch handles.exp.piezoMode
         end
         if handles.save.direct
             direct=zeros(size(data,1),size(data,2),handles.save.N);
-            for i=1:handles.save.N
+            for i=1:handles.save.N*handles.save.repeatN
                 direct(:,:,i)=mean(data(:,:,1,4*(i-1)*handles.octCam.Naccu+1:4:4*i*handles.octCam.Naccu),4);
             end
             saveAsTiff(direct,'direct','adimec',handles)
@@ -132,7 +138,7 @@ switch handles.exp.piezoMode
         end
     case 4 % 5 phases
 end
-
+saveParameters(handles)
 set(handles.octCam.vid, 'TriggerFrameDelay', 0)
-
+close(h)
 acq_state=0;

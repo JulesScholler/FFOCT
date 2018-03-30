3bdd7ca7bbe93fc210a4acb32667e995d16de0ac
Jules Scholler <jules.scholler@gmail.com>
2018-03-30 11:45:00 +0200

    modify COM port for motor

--- fun/initialisationMotors.m
+++ fun/initialisationMotors.m
@@ -6,10 +6,10 @@ function handles=initialisationMotors(handles)
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 if handles.gui.mode == 1
-    handles.motors.port = serial('com7','BaudRate',9600);
+    handles.motors.port = serial('com3','BaudRate',9600);
     fopen(handles.motors.port);
     handles.motors.protocol=Zaber.Protocol.detect(handles.motors.port);
-    % handles.motors.sample = Zaber.BinaryDevice.initialize(handles.motors.protocol, 4);
+    handles.motors.sample = Zaber.BinaryDevice.initialize(handles.motors.protocol, 4);
     handles.motors.ref = Zaber.BinaryDevice.initialize(handles.motors.protocol, 3);
     handles.motors.RefMode=get(handles.menuRefMotor,'Value');
     handles.motors.SampleMode=get(handles.menuSampleMotor,'Value');
